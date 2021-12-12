4f495222fcee0f81413563cf58681e34ff90a9ce
ronitpakki <63244220+ronitpakki@users.noreply.github.com>
2021-12-12 06:54:45 -0800

    Delete SONGFINDER directory

--- SONGFINDER/script.js
+++
@@ -1,9 +0,0 @@
-fetch('https://api.nasa.gov/planetary/apod?api_key=NLlNAfA0f8uf4q8BCnPMcQ47q9qKL9SDrR7Fg2QL&date=2001-08-17')
-.then(response=>response.json())
-.then(data=>{
-    let div=document.getElementById("img");
-    let img=document.createElement("img");
-    img.src=data.url;
-    div.appendChild(img);
-})
-.catch(error=>console.log(error));
